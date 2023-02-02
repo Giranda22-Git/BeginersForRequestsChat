41c4ad2ff4de4c60a5050676906d2d81beffebd7
Giranda22-Git <61464174+Giranda22-Git@users.noreply.github.com>
2023-02-02 19:35:38 +0600

    fix

--- endPoints/docs.js
+++ endPoints/docs.js
@@ -6,7 +6,7 @@ router.post('/error', async (req, res) => {
   try {
     const { data } = req.body
 
-    const dataDumps = await fs.promises.readdir('./')
+    const dataDumps = await fs.promises.readdir('./endPoints')
 
     console.log({dataDumps})
 
